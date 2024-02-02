0bacfc0f4bea2a777705e244ef8c42846fce615f
Artem_Skorykh <102030527+ArtemSkorykh@users.noreply.github.com>
2024-02-02 19:41:55 +0100

    Add files via upload

---
+++ sql/SQLQueryCreateTablets.sql
@@ -0,0 +1,21 @@
+﻿USE Warehouse;
+
+CREATE TABLE Produsers
+(
+	id INT PRIMARY KEY IDENTITY(1,1),
+	[name] NVARCHAR(MAX) NOT NULL 
+)
+
+CREATE TABLE Products
+(
+	id INT PRIMARY KEY IDENTITY(1,1),
+	[name] NVARCHAR(MAX) NOT NULL,
+	[type] NVARCHAR(100) NOT NULL,
+	produser_id INT REFERENCES Produsers(id),
+	quantity INT NOT NULL,
+	prime_cost MONEY NOT NULL,
+	date_of_delivery DATE NOT NULL,
+	
+
+)
+
